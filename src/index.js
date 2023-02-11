81e5f6a4d92c58a3c776cc43b2591fede218f6db
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-02-11 19:00:56 +0200

    test dynamical route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ app.get('/hello', (_, res) => {
   res.send('Hello World!');
 });
 
+app.get('/hello/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Your id is "${id}"`);
+})
+
 app.listen(PORT, () => {
   console.log(`Server is started on ${PORT} port`);
 });
