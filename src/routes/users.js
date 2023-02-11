309f95e5ea287e590cff9fe7d947e15627d5b9c4
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-02-11 19:33:55 +0200

    Users routes middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,11 @@ import { Router } from 'express';
 
 const userRoutes = Router();
 
+userRoutes.use((req, res, next) => {
+  console.log('You have sent request to users routes!');
+  next();
+});
+
 userRoutes.post('/login', (req, res) => {
   res.send('Login route');
 });
