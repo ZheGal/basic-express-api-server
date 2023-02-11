1d8f709e5119f0c0e0c64b0d526b8720fc4bfdb0
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-02-11 19:31:48 +0200

    basic routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import { router } from './routes/index.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
 
 app.get('/hello', (_, res) => {
   res.send('Hello World!');
@@ -12,7 +14,9 @@ app.get('/hello', (_, res) => {
 app.get('/hello/:id', (req, res) => {
     const { id } = req.params;
     res.send(`Your id is "${id}"`);
-})
+});
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server is started on ${PORT} port`);
