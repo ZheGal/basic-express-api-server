54719917ee4ff2a55c9f6b4c94309bfc5d4c2f16
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-02-12 08:54:47 +0200

    Typing Users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,17 +1,17 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 const userRoutes = Router();
 
-userRoutes.use((req, res, next) => {
+userRoutes.use((req: Request, res: Response, next: NextFunction) => {
   console.log('You have sent request to users routes!');
   next();
 });
 
-userRoutes.post('/login', (req, res) => {
+userRoutes.post('/login', (req: Request, res: Response) => {
   res.send('Login route');
 });
 
-userRoutes.post('/register', (req, res) => {
+userRoutes.post('/register', (req: Request, res: Response) => {
   res.send('Register route');
 });
 
